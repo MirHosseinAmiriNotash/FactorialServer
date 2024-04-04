bf0b01841173e2266ae699f934a185542cf52e0a
Hossein <mh.amiri.notash@gmail.com>
2024-04-04 19:10:46 +0330

    Connection between factorial server and factorial client is established.

--- FactorialClient.java
+++ FactorialClient.java
@@ -1,4 +1,13 @@
-public class FactorialClient {
+import java.net.Socket;
 
+public class FactorialClient {
+public static void main(String[] args) {
+    try{
+       System.out.println("Client Started...");
+       Socket soc = new Socket("localhost",9086);
+    }catch(Exception e){
+        e.printStackTrace();
+    }
+}//main
     
-}
+}//class
--- FactorialServer.java
+++ FactorialServer.java
@@ -1,3 +1,16 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class FactorialServer {
-    
-}
+    public static void main(String[] args) {
+        try {
+            System.out.println("Waiting for Clients...");
+            ServerSocket ss = new ServerSocket(9086);
+            Socket soc = ss.accept();
+            System.out.println("Connection Established.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }// main
+}// class
