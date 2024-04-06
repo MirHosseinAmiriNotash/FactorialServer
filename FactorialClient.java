9ee59f73c3121ade4222c2ff963b501514da27cf
Hossein <mh.amiri.notash@gmail.com>
2024-04-06 14:01:13 +0330

    factorial calculation Added

--- FactorialClient.java
+++ FactorialClient.java
@@ -1,13 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class FactorialClient {
-public static void main(String[] args) {
-    try{
-       System.out.println("Client Started...");
-       Socket soc = new Socket("localhost",9086);
-    }catch(Exception e){
-        e.printStackTrace();
-    }
-}//main
-    
-}//class
+    public static void main(String[] args) {
+        try {
+            System.out.println("Client Started...");
+            Socket soc = new Socket("localhost", 9086);
+            BufferedReader userinput = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Enter Number : ");
+            int number = Integer.parseInt(userinput.readLine());
+            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+            out.println(number);
+            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            System.out.println(in.readLine());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }// main
+
+}// class
--- FactorialServer.java
+++ FactorialServer.java
@@ -9,8 +9,21 @@ public class FactorialServer {
             ServerSocket ss = new ServerSocket(9086);
             Socket soc = ss.accept();
             System.out.println("Connection Established.");
+            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            int number = Integer.parseInt(in.readLine());
+            PrintWriter out = new PrintWriter(soc.getOutputStream(),true);
+            out.println("Factoral of " + number + " is : " + calculateFactorial(number));
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }// main
+    public static int calculateFactorial(int number) {
+        int fact = 1;
+        for (int i = 1; i <= number; i++) {
+            fact = fact * i;
+
+        }
+        return fact;
+    }//calculateFactorial
 }// class
